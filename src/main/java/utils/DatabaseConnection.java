a158bff4dc5d68a92886a86e340abd3da3242fe5
hunngphan06 <phanvogiahung@gmail.com>
2025-03-29 23:07:57 +0700

    Day

--- src/main/java/utils/DatabaseConnection.java
+++ src/main/java/utils/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/qlbvvnd?serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "quockhanh";
+    private static final String PASSWORD = "123456";
 
     public static Connection getConnection() throws SQLException {
         try {
